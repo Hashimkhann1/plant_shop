6215f052917ce76c72ec0156b5c60067007f1d10
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-03 20:43:35 +0500

    all app colors are added here

---
+++ lib/utils/app_colors/app_colors.dart
@@ -0,0 +1,15 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const Color primaryColor = Color(0xFF40744d);
+  static const Color lightGreenColor = Color(0xFFdae5de);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color greyColor = Colors.grey;
+
+}
