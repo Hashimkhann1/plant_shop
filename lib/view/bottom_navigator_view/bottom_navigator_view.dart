2ccae972de40990908916115cbca2d60b1cf2f73
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-05 23:18:25 +0500

    ui is completed

---
+++ lib/view/bottom_navigator_view/bottom_navigator_view.dart
@@ -0,0 +1,82 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:plant_shop/utils/app_colors/app_colors.dart';
+import 'package:plant_shop/view/favourite_item_view/favourite_item_view.dart';
+import 'package:plant_shop/view/home_view/home_view.dart';
+import 'package:plant_shop/view/notification_view/notification_view.dart';
+
+class BotttomNavigatorView extends StatefulWidget {
+  const BotttomNavigatorView({super.key});
+
+  @override
+  State<BotttomNavigatorView> createState() => _BotttomNavigatorViewState();
+}
+
+class _BotttomNavigatorViewState extends State<BotttomNavigatorView> {
+
+   List allViews = [
+   const HomeView(),
+    const FavouriteItemView(),
+    const NotificationView(),
+    const HomeView(),
+  ];
+
+   int selectedViewIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: allViews[selectedViewIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(20),
+                topLeft: Radius.circular(20)
+            ),
+          border: Border(top: BorderSide(color: Colors.grey.shade300)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(20),
+            topLeft: Radius.circular(20)
+          ),
+          child: Theme(
+            data: ThemeData(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+            ),
+            child: BottomNavigationBar(
+              currentIndex: selectedViewIndex,
+              onTap: (index) {
+                setState(() {
+                  selectedViewIndex = index;
+                });
+              },
+              type: BottomNavigationBarType.fixed,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              selectedItemColor: AppColors.primaryColor,
+              unselectedItemColor: AppColors.greyColor,
+              items: [
+                BottomNavigationBarItem(icon: Icon(selectedViewIndex == 0 ? CupertinoIcons.house_fill : CupertinoIcons.house),label: ''),
+                BottomNavigationBarItem(icon: Icon(selectedViewIndex == 1 ? CupertinoIcons.heart_fill : CupertinoIcons.heart,),label: ''),
+                BottomNavigationBarItem(icon: Icon(selectedViewIndex == 2 ? CupertinoIcons.bell_fill : CupertinoIcons.bell,),label: ''),
+                BottomNavigationBarItem(icon: Icon(selectedViewIndex == 3 ? CupertinoIcons.person_circle_fill : CupertinoIcons.profile_circled,),label: ''),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
