7855099c97b2d3f42fcb0fda4e9f8af6808f0fcd
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-08 23:04:47 +0500

    routes name

---
+++ lib/utils/view.dart
@@ -0,0 +1,16 @@
+
+export 'package:flutter/material.dart';
+export 'package:plant_shop/utils/routes/routes_name.dart';
+export 'package:plant_shop/utils/widgets/my_text.dart';
+export 'package:plant_shop/view/auth_view/sign_in_view/sign_in_view.dart';
+export 'package:plant_shop/view/auth_view/sign_up_view/sign_up_view.dart';
+export 'package:plant_shop/view/bottom_navigator_view/bottom_navigator_view.dart';
+export 'package:plant_shop/view/favourite_item_view/favourite_item_view.dart';
+export 'package:plant_shop/view/home_view/home_view.dart';
+export 'package:plant_shop/view/notification_view/notification_view.dart';
+export 'package:plant_shop/view/profile_view/profile_view.dart';
+export 'package:plant_shop/view/splash_view/splash_view.dart';
+export 'package:plant_shop/utils/app_colors/app_colors.dart';
+export 'package:plant_shop/utils/widgets/my_text_button.dart';
+export 'package:plant_shop/utils/widgets/my_text_form_field.dart';
+export 'package:plant_shop/view_model/auth_view_model/auth_view_model.dart';
