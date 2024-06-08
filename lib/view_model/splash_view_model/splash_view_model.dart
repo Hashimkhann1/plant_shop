6de2fb11ec367a847a8429bd9cc2e2bc81c23e9a
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-08 23:07:20 +0500

    navigator is changed

--- lib/view_model/splash_view_model/splash_view_model.dart
+++ lib/view_model/splash_view_model/splash_view_model.dart
@@ -1,15 +1,24 @@
 
 
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:plant_shop/utils/routes/routes_name.dart';
+import 'package:plant_shop/view/auth_view/sign_in_view/sign_in_view.dart';
 import 'package:plant_shop/view/bottom_navigator_view/bottom_navigator_view.dart';
 
 class SplashViewModel {
 
+  final _auth = FirebaseAuth.instance;
+
   void splashTime(BuildContext context) {
     Future.delayed(Duration(seconds: 4), () {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => BotttomNavigatorView()));
+      if(_auth.currentUser != null) {
+        Navigator.pushNamedAndRemoveUntil(context, RoutesName.bottomNavigatorView, (route) => false);
+      }else{
+        Navigator.pushNamedAndRemoveUntil(context, RoutesName.signInView, (route) => false);
+      }
     });
   }
 
