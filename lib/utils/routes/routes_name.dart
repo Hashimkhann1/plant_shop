f7b7146df23b7489fa7b3058f30805ae0e23ced8
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-09 22:50:21 +0500

    routes names

---
+++ lib/utils/routes/routes_name.dart
@@ -0,0 +1,18 @@
+
+
+
+class RoutesName {
+
+  static const String splashView = 'splash_view';
+  static const String signInView = 'sign_in_view';
+  static const String signUpView = 'sign_up_view';
+  static const String bottomNavigatorView = 'bottom_navigator_view';
+  static const String homeView = 'home_view';
+  static const String itemDetailView = 'item_detail_view';
+  static const String favouriteView = 'favourite_view';
+  static const String notificationView = 'notification_view';
+  static const String profileView = 'profile_view';
+
+
+
+}
