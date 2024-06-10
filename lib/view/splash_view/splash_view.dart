850065e787323e437403a9e1a6197ec761ea7351
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-10 23:12:39 +0500

    ui is completed

--- lib/view/splash_view/splash_view.dart
+++ lib/view/splash_view/splash_view.dart
@@ -1,5 +1,6 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:plant_shop/utils/app_colors/app_colors.dart';
 import 'package:plant_shop/view_model/splash_view_model/splash_view_model.dart';
 
 class SplashView extends StatefulWidget {
@@ -14,52 +15,46 @@ class _SplashViewState extends State<SplashView> {
   void initState() {
     super.initState();
     // Preload the image
-    _preloadImage();
 
     SplashViewModel().splashTime(context);
-  }
-
-  void _preloadImage() {
-    final image = AssetImage('images/plant_splash.jpeg');
-    image.resolve(const ImageConfiguration()).addListener(
-      ImageStreamListener(
-            (info, call) {
-          // Image loaded successfully
-          setState(() {});
-        },
-        onError: (error, stackTrace) {
-          // Handle error
-        },
-      ),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        width: MediaQuery.sizeOf(context).width,
-        height: MediaQuery.sizeOf(context).height,
-        decoration: BoxDecoration(
-          color: Color(0xFF1c4642),
-          image: DecorationImage(
-            image: AssetImage('images/plant_splash.jpeg'),
-            fit: BoxFit.fitHeight,
-            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
-          ),
-        ),
-        child: DefaultTextStyle(
-          style: const TextStyle(
-            fontSize: 26.0,
-            fontWeight: FontWeight.w700,
-          ),
-          child: AnimatedTextKit(
-            animatedTexts: [
-              WavyAnimatedText('Green Vibes'),
-            ],
-            isRepeatingAnimation: true,
-          ),
+      backgroundColor: AppColors.lightGreenColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.sizeOf(context).width,
+              height: MediaQuery.sizeOf(context).height * 0.40,
+              decoration: BoxDecoration(
+                // color: Color(0xFF1c4642),
+                image: DecorationImage(
+                  image: AssetImage('images/trans.png',),
+                  fit: BoxFit.contain,
+                  // colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+                ),
+              ),
+            ),
+            DefaultTextStyle(
+              style: const TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w700,
+                  color: AppColors.primaryColor
+              ),
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  WavyAnimatedText('Green View'),
+                ],
+                isRepeatingAnimation: true,
+              ),
+            ),
+          ],
         ),
       ),
     );
