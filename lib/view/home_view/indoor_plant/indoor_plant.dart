38e0a14cc9f781e621cce67e76d8dba444295483
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-12 21:56:12 +0500

    responcive is completed

--- lib/view/home_view/indoor_plant/indoor_plant.dart
+++ lib/view/home_view/indoor_plant/indoor_plant.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:plant_shop/utils/app_colors/app_colors.dart';
+import 'package:plant_shop/utils/responsive/responsive.dart';
 import 'package:plant_shop/utils/widgets/my_text.dart';
 import 'package:plant_shop/view/item_detail_view/item_detail_view.dart';
 
@@ -17,15 +18,16 @@ class IndoorPlant extends StatelessWidget {
     return GridView.builder(
       itemCount: AppColors.plants.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          childAspectRatio: 2/2.3,
+            crossAxisCount: Responsive.isMobile(context) ? 2 : MediaQuery.of(context).size.width < 1241 ? 3 : 4,
+          childAspectRatio: Responsive.isMobile(context) ? 2/2.3 : Responsive.isTablet(context) ? 1.8/1.7 : MediaQuery.of(context).size.width < 1555 ? 1.8/1.6 : 1.7/1.2,
           crossAxisSpacing: 6,
           mainAxisSpacing: 6
         ),
         itemBuilder: (context , index) {
+
           return InkWell(
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => ItemDetailView(itemImage: "${AppColors.plants[index]['plantImage']}${index+1}.jpeg",heroTag: "detailItem${index}",)));
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ItemDetailView(itemImage: "${AppColors.plants[index]['plantImage']}${index+1}.jpeg",heroTag: "detailItem$index",)));
             },
             child: Card(
               color: AppColors.whiteColor,
@@ -33,14 +35,14 @@ class IndoorPlant extends StatelessWidget {
               child: Column(
                 children: [
                   /// plant image
-                  Hero(tag: 'detailItem${index}', child: Image.asset("${AppColors.plants[index]['plantImage']}${index+1}.jpeg",fit: BoxFit.cover,height: 136,)),
+                  Hero(tag: 'detailItem$index', child: Image.asset("${AppColors.plants[index]['plantImage']}${index+1}.jpeg",fit: BoxFit.cover,height: Responsive.isMobile(context) ? 136 : height * 0.15,)),
 
-                  /// plant Nmae
-                  MyText(title: "Bambo Plant",fontWeight: FontWeight.w500,),
+                  /// plant name
+                  const MyText(title: "Bamboo Plant",fontWeight: FontWeight.w500,),
                   SizedBox(height: height * 0.003,),
 
                   /// plant price
-                  MyText(title: "\$48",fontSize: 13,)
+                  const MyText(title: "\$48",fontSize: 13,)
                 ],
               ),
             ),
