e2d63f1b372d717855f31e432b42e5fab4e68b16
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-12 21:55:28 +0500

    responcivness is completed for mobile , tablet and web

--- lib/view/auth_view/sign_up_view/sign_up_view.dart
+++ lib/view/auth_view/sign_up_view/sign_up_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:plant_shop/utils/responsive/responsive.dart';
 import 'package:plant_shop/view_model/bloc/loadin_bloc/loading_bloc/loading_bloc.dart';
 import 'package:plant_shop/view_model/bloc/loadin_bloc/loading_bloc_state/loading_bloc_state.dart';
 
@@ -23,209 +24,214 @@ class SignUpView extends StatelessWidget {
         backgroundColor: AppColors.whiteColor,
         body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: height * 0.02,
-                ),
-                Column(
+            child: Center(
+              child: SizedBox(
+                width: Responsive.isMobile(context) ? width : width * 0.32,
+                child: Column(
                   children: [
+                    SizedBox(
+                      height: Responsive.isMobile(context) ? height * 0.02 : height * 0.1,
+                    ),
+                    Column(
+                      children: [
 
-                    /// back button
-                    Padding(
-                      padding: const EdgeInsets.only(left: 22.0),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: InkWell(
-                          onTap: () => Navigator.pop(context),
-                          child: Container(
-                            padding: const EdgeInsets.all(6),
-                            decoration: const BoxDecoration(
-                              color: AppColors.lightGreenColor,
-                              shape: BoxShape.circle
+                        /// back button
+                        Padding(
+                          padding: const EdgeInsets.only(left: 22.0),
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: InkWell(
+                              onTap: () => Navigator.pop(context),
+                              child: Container(
+                                padding: const EdgeInsets.all(6),
+                                decoration: const BoxDecoration(
+                                  color: AppColors.lightGreenColor,
+                                  shape: BoxShape.circle
+                                ),
+                                child: const Icon(Icons.arrow_back_ios_new_outlined,size: 22,),
+                              ),
                             ),
-                            child: const Icon(Icons.arrow_back_ios_new_outlined,size: 22,),
                           ),
                         ),
-                      ),
-                    ),
 
-                    Container(
-                      height: height * 0.14,
-                      child: Hero(
-                        tag: 'sigin_hero',
-                          child: Image.asset('images/sigin_leaf.png',fit: BoxFit.contain,)),
-                    ),
+                        Container(
+                          height: height * 0.14,
+                          child: Hero(
+                            tag: 'sigin_hero',
+                              child: Image.asset('images/sigin_leaf.png',fit: BoxFit.contain,)),
+                        ),
 
-                    const MyText(
-                      title: "Register",
-                      color: AppColors.primaryColor,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    const MyText(
-                      title: "Create your new account",
-                      color: Colors.grey,
-                      fontSize: 15,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: height * 0.06,
-                ),
-
-                /// form
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * 0.04),
-                  child: Form(
-                    key: _formKey,
-                      child: Column(
-                    children: [
-                      /// first name textFormField
-                      MyTextFormFeild(
-                        controller: nameController,
-                        prefixIcon: const Icon(Icons.person,color: AppColors.primaryColor,),                        hintText: "First name",
-                        fillColor: AppColors.lightGreenColor,
-                        borderRadius: 12,
-                        borderSide:
-                            const BorderSide(color: AppColors.lightGreenColor),
-                        validator: (value) {
-                          if(value!.isEmpty){
-                            return "Enter name";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: height * 0.02,
-                      ),
-
-                      /// email textFormField
-                      MyTextFormFeild(
-                        controller: emailController,
-                        prefixIcon: const Icon(Icons.email,color: AppColors.primaryColor,),
-                        hintText: "Email",
-                        fillColor: AppColors.lightGreenColor,
-                        borderRadius: 12,
-                        borderSide:
-                            const BorderSide(color: AppColors.lightGreenColor),
-                        validator: (value) {
-                          if(value!.isEmpty){
-                            return "Enter email";
-                          }else if(!value.contains("@gmail.com")){
-                            return "Invalid email";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: height * 0.02,
-                      ),
-
-                      /// password textFormField
-                      MyTextFormFeild(
-                        controller: passwordController,
-                          obscureText: true,
-                          prefixIcon: const Icon(Icons.lock,color: AppColors.primaryColor,),
-                          hintText: "Password",
-                          fillColor: AppColors.lightGreenColor,
-                          borderRadius: 12,
-                          borderSide:
-                          const BorderSide(color: AppColors.lightGreenColor),
-                        validator: (value) {
-                          if(value!.isEmpty){
-                            return "Enter password";
-                          }else if(value.length <= 6){
-                            return "Password must be grather than 6 charcaters";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: height * 0.02,
-                      ),
-
-                      /// confirm password textFormField
-                      MyTextFormFeild(
-                        controller: confirmController,
-                          obscureText: true,
-                          prefixIcon: const Icon(Icons.lock,color: AppColors.primaryColor,),
-                          hintText: "Confirm password",
-                          fillColor: AppColors.lightGreenColor,
-                          borderRadius: 12,
-                          borderSide:
-                          const BorderSide(color: AppColors.lightGreenColor),
-                        validator: (value) {
-                          if(value!.isEmpty){
-                            return "Enter confirm password";
-                          }else if(value != passwordController.text){
-                            return "Comnfirm password not matched";
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: height * 0.004,
-                      ),
-
-                    ],
-                  )),
-                ),
-                SizedBox(
-                  height: height * 0.05,
-                ),
-
-                /// sign up and already have an accounty buttons
-                Column(
-                  children: [
-                    /// sign up button
-                    BlocBuilder<LoadingBloc , LoadingBlocState>(builder: (context , state) {
-                      return MyTextButton(
-                        title: "Sign up",
-                        textColor: AppColors.whiteColor,
-                        fontSize: 17,
-                        fontWeight: FontWeight.w600,
-                        backgroundColor: AppColors.primaryColor,
-                        borderRadius: 30,
-                        width: width * 0.90,
-                        height: 46,
-                        alignment: Alignment.center,
-                        isLoading: state.isLoading,
-                        onTap: () {
-                          if(_formKey.currentState!.validate()){
-                            AuthViewModel().registerUser(context, nameController.text, emailController.text, passwordController.text);
-                          }
-                        },
-                      );
-                    }),
-                    SizedBox(
-                      height: height * 0.02,
-                    ),
-
-                    /// dont't have an account
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
                         const MyText(
-                          title: "Already have an account?",
-                          color: AppColors.greyColor,
+                          title: "Register",
+                          color: AppColors.primaryColor,
+                          fontSize: 32,
+                          fontWeight: FontWeight.w500,
                         ),
-                        const SizedBox(
-                          width: 6,
+                        const MyText(
+                          title: "Create your new account",
+                          color: Colors.grey,
+                          fontSize: 15,
                         ),
-                        MyTextButton(
-                          title: "Sign in",
-                          textColor: AppColors.primaryColor,
-                          fontWeight: FontWeight.w600,
-                          onTap: (){
-                            Navigator.pop(context);
-                          },
+                      ],
+                    ),
+                    SizedBox(
+                      height: height * 0.06,
+                    ),
+
+                    /// form
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width * 0.04),
+                      child: Form(
+                        key: _formKey,
+                          child: Column(
+                        children: [
+                          /// first name textFormField
+                          MyTextFormFeild(
+                            controller: nameController,
+                            prefixIcon: const Icon(Icons.person,color: AppColors.primaryColor,),                        hintText: "First name",
+                            fillColor: AppColors.lightGreenColor,
+                            borderRadius: 12,
+                            borderSide:
+                                const BorderSide(color: AppColors.lightGreenColor),
+                            validator: (value) {
+                              if(value!.isEmpty){
+                                return "Enter name";
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(
+                            height: height * 0.02,
+                          ),
+
+                          /// email textFormField
+                          MyTextFormFeild(
+                            controller: emailController,
+                            prefixIcon: const Icon(Icons.email,color: AppColors.primaryColor,),
+                            hintText: "Email",
+                            fillColor: AppColors.lightGreenColor,
+                            borderRadius: 12,
+                            borderSide:
+                                const BorderSide(color: AppColors.lightGreenColor),
+                            validator: (value) {
+                              if(value!.isEmpty){
+                                return "Enter email";
+                              }else if(!value.contains("@gmail.com")){
+                                return "Invalid email";
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(
+                            height: height * 0.02,
+                          ),
+
+                          /// password textFormField
+                          MyTextFormFeild(
+                            controller: passwordController,
+                              obscureText: true,
+                              prefixIcon: const Icon(Icons.lock,color: AppColors.primaryColor,),
+                              hintText: "Password",
+                              fillColor: AppColors.lightGreenColor,
+                              borderRadius: 12,
+                              borderSide:
+                              const BorderSide(color: AppColors.lightGreenColor),
+                            validator: (value) {
+                              if(value!.isEmpty){
+                                return "Enter password";
+                              }else if(value.length <= 6){
+                                return "Password must be grather than 6 charcaters";
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(
+                            height: height * 0.02,
+                          ),
+
+                          /// confirm password textFormField
+                          MyTextFormFeild(
+                            controller: confirmController,
+                              obscureText: true,
+                              prefixIcon: const Icon(Icons.lock,color: AppColors.primaryColor,),
+                              hintText: "Confirm password",
+                              fillColor: AppColors.lightGreenColor,
+                              borderRadius: 12,
+                              borderSide:
+                              const BorderSide(color: AppColors.lightGreenColor),
+                            validator: (value) {
+                              if(value!.isEmpty){
+                                return "Enter confirm password";
+                              }else if(value != passwordController.text){
+                                return "Comnfirm password not matched";
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(
+                            height: height * 0.004,
+                          ),
+
+                        ],
+                      )),
+                    ),
+                    SizedBox(
+                      height: height * 0.05,
+                    ),
+
+                    /// sign up and already have an accounty buttons
+                    Column(
+                      children: [
+                        /// sign up button
+                        BlocBuilder<LoadingBloc , LoadingBlocState>(builder: (context , state) {
+                          return MyTextButton(
+                            title: "Sign up",
+                            textColor: AppColors.whiteColor,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w600,
+                            backgroundColor: AppColors.primaryColor,
+                            borderRadius: 30,
+                            width: Responsive.isMobile(context) ? width * 0.90 : width * 0.24,
+                            height: 46,
+                            alignment: Alignment.center,
+                            isLoading: state.isLoading,
+                            onTap: () {
+                              if(_formKey.currentState!.validate()){
+                                AuthViewModel().registerUser(context, nameController.text, emailController.text, passwordController.text);
+                              }
+                            },
+                          );
+                        }),
+                        SizedBox(
+                          height: height * 0.02,
+                        ),
+
+                        /// dont't have an account
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const MyText(
+                              title: "Already have an account?",
+                              color: AppColors.greyColor,
+                            ),
+                            const SizedBox(
+                              width: 6,
+                            ),
+                            MyTextButton(
+                              title: "Sign in",
+                              textColor: AppColors.primaryColor,
+                              fontWeight: FontWeight.w600,
+                              onTap: (){
+                                Navigator.pop(context);
+                              },
+                            )
+                          ],
                         )
                       ],
                     )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
           ),
         ));
