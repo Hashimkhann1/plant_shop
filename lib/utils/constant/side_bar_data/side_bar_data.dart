2166667fe6b2aa0630e61e70c149188172186931
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-12 21:53:28 +0500

    side bar is added for web

---
+++ lib/utils/constant/side_bar_data/side_bar_data.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class SideBarData {
+
+  final data = [
+    {'title':'Home','icon':CupertinoIcons.house , 'fillIcon' : CupertinoIcons.house_fill},
+    {'title':'Favourite','icon':CupertinoIcons.heart,'fillIcon' : CupertinoIcons.heart_fill},
+    {'title':'Notification','icon':CupertinoIcons.bell,'fillIcon' : CupertinoIcons.bell_fill},
+    {'title':'Add Item','icon':CupertinoIcons.add,'fillIcon' : CupertinoIcons.add_circled_solid},
+    {'title':'Profile','icon':CupertinoIcons.person_circle,'fillIcon' : CupertinoIcons.person_circle_fill}
+  ];
+
+}
