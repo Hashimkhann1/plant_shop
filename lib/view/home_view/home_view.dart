5b0f7485460de9dfc3e223ea70045ea481b356f6
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-12 21:55:55 +0500

    responcive is completed

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:plant_shop/utils/app_colors/app_colors.dart';
+import 'package:plant_shop/utils/responsive/responsive.dart';
 import 'package:plant_shop/utils/widgets/my_appbar_widget.dart';
 import 'package:plant_shop/utils/widgets/my_text.dart';
 import 'package:plant_shop/utils/widgets/my_text_form_field.dart';
+import 'package:plant_shop/utils/widgets/side_bar_widget/side_bar_widget.dart';
 import 'package:plant_shop/view/home_view/indoor_plant/indoor_plant.dart';
 
 class HomeView extends StatefulWidget {
@@ -37,65 +38,76 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
 
     return Scaffold(
       backgroundColor: AppColors.whiteColor,
-      appBar: MyAppBarWidget(),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: width * 0.03),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: height * 0.03,),
+      appBar: const MyAppBarWidget(automaticallyImplyLeading: true,),
+      body: Row(
+        children: [
+          !Responsive.isDesktop(context) ? const SizedBox() : Expanded(
+            flex: 2,
+            child: SideBarWidget()
+          ),
+          Expanded(
+            flex: 8,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.03),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: height * 0.03,),
 
-            /// choose you fav plant text
-            Container(
-                width: width * 0.43,
-                child: MyText(title: "Choose Your Favourite Plant",maxLines: 2,fontSize: 20,fontWeight: FontWeight.bold,)
-            ),
-            SizedBox(height: height * 0.02,),
+                  /// choose you fav plant text
+                  SizedBox(
+                      width: width * 0.43,
+                      child: const MyText(title: "Choose Your Favourite Plant",maxLines: 2,fontSize: 20,fontWeight: FontWeight.bold,)
+                  ),
+                  SizedBox(height: height * 0.02,),
 
-            /// search text form field
-            MyTextFormFeild(hintText: "Search",fillColor: Color(0xFFf2f2f2),borderSide: BorderSide(color: Colors.grey.shade100),focusedBorderColor: Colors.grey.shade100,),
-            SizedBox(height: height * 0.02,),
+                  /// search text form field
+                  MyTextFormFeild(hintText: "Search",fillColor: const Color(0xFFf2f2f2),borderSide: BorderSide(color: Colors.grey.shade100),focusedBorderColor: Colors.grey.shade100,),
+                  SizedBox(height: height * 0.02,),
 
-            /// tab bar
-            Container(
-              height: height * 0.044,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(20)
-              ),
-              child: TabBar(
-                controller: _tabController,
-                indicatorSize: TabBarIndicatorSize.tab,
-                indicator: BoxDecoration(
-                  color: AppColors.primaryColor,
-                  borderRadius: BorderRadius.circular(20)
-                ),
-                labelColor: AppColors.whiteColor,
-                unselectedLabelColor: AppColors.primaryColor,
-                dividerColor: Colors.transparent,
-                splashFactory: NoSplash.splashFactory,
-                tabs: [
-                  Tab(child: MyText(title: "Indoor Plant"),),
-                  Tab(child: MyText(title: "Flower"),),
-                  Tab(child: MyText(title: "Tree"),),
+                  /// tab bar
+                  Container(
+                    height: height * 0.044,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: TabBar(
+                      controller: _tabController,
+                      indicatorSize: TabBarIndicatorSize.tab,
+                      indicator: BoxDecoration(
+                        color: AppColors.primaryColor,
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      labelColor: AppColors.whiteColor,
+                      unselectedLabelColor: AppColors.primaryColor,
+                      dividerColor: Colors.transparent,
+                      splashFactory: NoSplash.splashFactory,
+                      tabs: const [
+                        Tab(child: MyText(title: "Indoor Plant"),),
+                        Tab(child: MyText(title: "Flower"),),
+                        Tab(child: MyText(title: "Tree"),),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: height * 0.02,),
+
+                  /// tab View
+                  Expanded(
+                    child: TabBarView(
+                      controller: _tabController,
+                        children: const [
+                          IndoorPlant(),
+                          MyText(title: "2"),
+                          MyText(title: "3"),
+                        ]
+                    ),
+                  )
                 ],
               ),
             ),
-            SizedBox(height: height * 0.02,),
-
-            /// tab View
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                  children: [
-                    IndoorPlant(),
-                    MyText(title: "2"),
-                    MyText(title: "3"),
-                  ]
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
