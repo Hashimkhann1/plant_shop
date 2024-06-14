5b6dc3d056458eb4e6b4763e0e3484e825cc8f97
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-14 21:56:50 +0500

    routes is added

--- lib/utils/constant/side_bar_data/side_bar_data.dart
+++ lib/utils/constant/side_bar_data/side_bar_data.dart
@@ -1,15 +1,16 @@
 
 
 import 'package:flutter/cupertino.dart';
+import 'package:plant_shop/utils/routes/routes_name.dart';
 
 class SideBarData {
 
   final data = [
-    {'title':'Home','icon':CupertinoIcons.house , 'fillIcon' : CupertinoIcons.house_fill},
-    {'title':'Favourite','icon':CupertinoIcons.heart,'fillIcon' : CupertinoIcons.heart_fill},
-    {'title':'Notification','icon':CupertinoIcons.bell,'fillIcon' : CupertinoIcons.bell_fill},
-    {'title':'Add Item','icon':CupertinoIcons.add,'fillIcon' : CupertinoIcons.add_circled_solid},
-    {'title':'Profile','icon':CupertinoIcons.person_circle,'fillIcon' : CupertinoIcons.person_circle_fill}
+    {'title':'Home','icon':CupertinoIcons.house , 'fillIcon' : CupertinoIcons.house_fill , 'routesName' : RoutesName.homeView},
+    {'title':'Favourite','icon':CupertinoIcons.heart,'fillIcon' : CupertinoIcons.heart_fill, 'routesName' : RoutesName.favouriteView},
+    {'title':'Notification','icon':CupertinoIcons.bell,'fillIcon' : CupertinoIcons.bell_fill, 'routesName' : RoutesName.notificationView},
+    {'title':'Add Item','icon':CupertinoIcons.add,'fillIcon' : CupertinoIcons.add_circled_solid, 'routesName' : RoutesName.addItemview},
+    {'title':'Profile','icon':CupertinoIcons.person_circle,'fillIcon' : CupertinoIcons.person_circle_fill, 'routesName' : RoutesName.profileView}
   ];
 
 }
