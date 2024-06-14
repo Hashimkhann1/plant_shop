6bbdc4a2e8f408335c1ab5c23b5ebe3af5128815
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-14 21:57:31 +0500

    add item rouet name  is added

--- lib/utils/routes/routes_name.dart
+++ lib/utils/routes/routes_name.dart
@@ -8,6 +8,7 @@ class RoutesName {
   static const String signUpView = 'sign_up_view';
   static const String bottomNavigatorView = 'bottom_navigator_view';
   static const String homeView = 'home_view';
+  static const String addItemview = 'add_item_view';
   static const String itemDetailView = 'item_detail_view';
   static const String favouriteView = 'favourite_view';
   static const String notificationView = 'notification_view';
