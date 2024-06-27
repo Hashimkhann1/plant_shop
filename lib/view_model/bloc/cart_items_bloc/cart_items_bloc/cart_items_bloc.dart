0af616c475cde73422f4954c40c7d8170b60aca4
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-27 15:44:04 +0500

    cart items bloc

---
+++ lib/view_model/bloc/cart_items_bloc/cart_items_bloc/cart_items_bloc.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:bloc/bloc.dart';
+import 'package:plant_shop/view_model/bloc/cart_items_bloc/cart_items_bloc_event/cart_items_bloc_event.dart';
+import 'package:plant_shop/view_model/bloc/cart_items_bloc/cart_items_bloc_state/cart_items_bloc_state.dart';
+
+class CartItemsBloc extends Bloc<CartItemsBlocEvent , CartItemsBlocState> {
+
+  final List<Map<String,String>> cartItems = [];
+
+  CartItemsBloc() : super(CartItemsBlocState()) {
+
+    on<AddItemToCart>(_addItemToCart);
+    on<RemoveItemFromCart>(_removeItemFromCart);
+
+  }
+
+  void _addItemToCart(AddItemToCart event , Emitter emit) {
+    cartItems.add(event.cartItem);
+    emit(state.copyWith(cartItems: List.from(cartItems)));
+  }
+
+  void _removeItemFromCart(RemoveItemFromCart event , Emitter emit) {}
+
+
+}
