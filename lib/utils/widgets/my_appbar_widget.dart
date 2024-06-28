35e30266e3a01d7d5f17e0e609096b74babdb8e2
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-28 07:00:52 +0500

    updated for cart

--- lib/utils/widgets/my_appbar_widget.dart
+++ lib/utils/widgets/my_appbar_widget.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:plant_shop/utils/app_colors/app_colors.dart';
+import 'package:plant_shop/utils/routes/routes_name.dart';
+import 'package:plant_shop/utils/widgets/my_text.dart';
 
 class MyAppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   final bool automaticallyImplyLeading;
-  const MyAppBarWidget({super.key,this.automaticallyImplyLeading = false});
+  final bool showCart;
+  const MyAppBarWidget({super.key,this.automaticallyImplyLeading = false , this.showCart = true});
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +23,22 @@ class MyAppBarWidget extends StatelessWidget implements PreferredSizeWidget {
         fit: BoxFit.contain,
       ),
       actions: [
-        IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.shopping_cart))
+        showCart ? Stack(
+          children: [
+            Positioned(
+              top: -3,
+              right: 17,
+              child: Container(
+                child: MyText(title: '0',fontSize: 17,),
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, RoutesName.cart_view);
+                },
+                icon: Icon(CupertinoIcons.shopping_cart))
+          ],
+        ) : SizedBox(),
       ],
     );
   }
