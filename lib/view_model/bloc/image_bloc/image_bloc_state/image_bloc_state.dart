377d9e3892150cb07aec3bf0c5465632ccc0fdd9
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-28 07:02:51 +0500

    working to pick image

--- lib/view_model/bloc/image_bloc/image_bloc_state/image_bloc_state.dart
+++ lib/view_model/bloc/image_bloc/image_bloc_state/image_bloc_state.dart
@@ -1,16 +1,18 @@
 
 
 
+import 'dart:typed_data';
+
 import 'package:equatable/equatable.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageBlocState extends Equatable {
 
-  final XFile? imagePath;
+   final Uint8List? imagePath;
 
   const ImageBlocState({this.imagePath});
 
-  ImageBlocState copyWith({XFile? imagePath}){
+  ImageBlocState copyWith({Uint8List? imagePath}){
     return ImageBlocState(imagePath: imagePath ?? this.imagePath);
   }
 
