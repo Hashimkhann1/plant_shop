5e1d900d83e5f604ade28a0be85b03f181883026
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-28 07:02:37 +0500

    working to pick image

--- lib/view_model/bloc/image_bloc/image_bloc/image_bloc.dart
+++ lib/view_model/bloc/image_bloc/image_bloc/image_bloc.dart
@@ -1,18 +1,25 @@
 
 
 import 'package:bloc/bloc.dart';
+import 'package:plant_shop/utils/constant/image_picker_utils/image_picker_utils.dart';
 import 'package:plant_shop/view_model/bloc/image_bloc/image_bloc_event/image_bloc_event.dart';
 import 'package:plant_shop/view_model/bloc/image_bloc/image_bloc_state/image_bloc_state.dart';
 
 class ImageBloc extends Bloc<ImageBlocEvent , ImageBlocState> {
 
-  ImageBloc() : super(ImageBlocState()){
-    on<PickImageFromGallery>(_pickImageFromGallery)
+  final ImagePickerUtils imagePickerUtils;
+
+  ImageBloc(this.imagePickerUtils) : super(ImageBlocState()){
+    on<PickImageFromGallery>(_pickImageFromGallery);
   }
 
 
-  void _pickImageFromGallery(PickImageFromGallery event, Emitter emit){
+  void _pickImageFromGallery(PickImageFromGallery event, Emitter emit) async {
 
+    final imagePath = await imagePickerUtils.pickImage();
+    var f = await imagePath?.readAsBytes();
+
+    emit(state.copyWith(imagePath: f));
   }
 
 }
