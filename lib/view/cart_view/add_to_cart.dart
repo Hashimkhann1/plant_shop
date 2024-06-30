e9632f639e47bfde4f7f51d6b77796d657035f47
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-06-30 23:06:48 +0500

    cart ui is complete

---
+++ lib/view/cart_view/add_to_cart.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:plant_shop/utils/app_colors/app_colors.dart';
+import 'package:plant_shop/utils/responsive/responsive.dart';
+import 'package:plant_shop/utils/routes/routes_name.dart';
+import 'package:plant_shop/utils/widgets/my_appbar_widget.dart';
+import 'package:plant_shop/utils/widgets/my_text.dart';
+
+import '../../utils/widgets/my_text_button.dart';
+
+class CartView extends StatelessWidget {
+  const CartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: const MyAppBarWidget(
+        showCart: false,
+        automaticallyImplyLeading: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: width * 0.02),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// cart title
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.02),
+              child: const MyText(
+                title: "Cart",
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+                color: AppColors.primaryColor,
+              ),
+            ),
+
+            /// cart items
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 7,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: AppColors.whiteColor,
+                      shadowColor: AppColors.whiteColor,
+                      elevation: 10,
+                      child: Row(
+                        children: [
+                          /// item image
+                          ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Image.asset(
+                                "${AppColors.plants[1]['plantImage']}${1}.jpeg",
+                                width: Responsive.isDesktop(context)
+                                    ? width * 0.11
+                                    : width * 0.30,
+                                height: Responsive.isDesktop(context)
+                                    ? height * 0.12
+                                    : height * 0.13,
+                                fit: BoxFit.contain,
+                              )),
+
+                          /// name price and descripition
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 4, horizontal: 6),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const MyText(
+                                        title: "Bamboo Plant",
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.black54,
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                            right: Responsive.isDesktop(context)
+                                                ? 12
+                                                : 8),
+                                        child: const MyText(
+                                          title: "\$30",
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500,
+                                          color: AppColors.primaryColor,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        width: Responsive.isDesktop(context) ||
+                                                Responsive.isTablet(context)
+                                            ? width * 0.11
+                                            : width * 0.20,
+                                        margin: const EdgeInsets.only(
+                                            right: 12.0, top: 8.0),
+                                        decoration: BoxDecoration(
+                                            color: AppColors.primaryColor,
+                                            borderRadius:
+                                                BorderRadius.circular(20)),
+                                        child: const Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            Icon(
+                                              Icons.remove,
+                                              color: AppColors.whiteColor,
+                                            ),
+                                            MyText(
+                                              title: "3",
+                                              fontSize: 15,
+                                              color: AppColors.whiteColor,
+                                            ),
+                                            Icon(
+                                              Icons.add,
+                                              color: AppColors.whiteColor,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      const Icon(
+                                        Icons.delete_outline,
+                                        color: AppColors.primaryColor,
+                                      ),
+                                    ],
+                                  ),
+                                  // MyText(title: "In publishing and graphic design, Lorem ipsum is a placeholder..." ,maxLines: 2,textAlign: TextAlign.start,overflow: TextOverflow.ellipsis,color: AppColors.greyColor,)
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+
+            /// buy button
+            Padding(
+              padding: const EdgeInsets.only(bottom: 12.0),
+              child: MyTextButton(
+                title: "Buy",
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                textColor: AppColors.whiteColor,
+                backgroundColor: AppColors.primaryColor,
+                width: width,
+                height: height * 0.06,
+                borderRadius: 12,
+                onTap: () {
+                  Navigator.pushNamed(context, RoutesName.buyer_info_view);
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
