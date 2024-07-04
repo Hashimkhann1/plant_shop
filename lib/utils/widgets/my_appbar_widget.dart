0782f54ca3f94e429acf65b2596b9d9eb1ce7bad
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-07-04 23:17:57 +0500

    block builder is added for cart itmes length

--- lib/utils/widgets/my_appbar_widget.dart
+++ lib/utils/widgets/my_appbar_widget.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:plant_shop/utils/app_colors/app_colors.dart';
 import 'package:plant_shop/utils/routes/routes_name.dart';
 import 'package:plant_shop/utils/widgets/my_text.dart';
+import 'package:plant_shop/view_model/bloc/cart_items_bloc/cart_items_bloc/cart_items_bloc.dart';
+import 'package:plant_shop/view_model/bloc/cart_items_bloc/cart_items_bloc_state/cart_items_bloc_state.dart';
 
 class MyAppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   final bool automaticallyImplyLeading;
@@ -26,10 +29,12 @@ class MyAppBarWidget extends StatelessWidget implements PreferredSizeWidget {
         showCart ? Stack(
           children: [
             Positioned(
-              top: -3,
+              top: -4,
               right: 17,
               child: Container(
-                child: MyText(title: '0',fontSize: 17,),
+                child: BlocBuilder<CartItemsBloc , CartItemsBlocState>(builder: (context , state) {
+                  return MyText(title: state.cartItems.length.toString(),fontSize: 17,);
+                },),
               ),
             ),
             IconButton(
